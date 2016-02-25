86ba30e9329048e726f777cf96e28efa7a1e070c
Live session user <kting93@gmail.com>
2016-02-25 23:15:54 +0000

    first commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+openmp.out: openMp_test.c
+	gcc -fopenmp openMp_test.c -o openmp.out
---
+++ openMp_test.c
@@ -0,0 +1,20 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+	int nthreads, tid;
+
+	#pragma omp parallel private(nthreads, tid)
+	{
+
+	tid = omp_get_thread_num();
+	printf("Hello World from thead = %d\n", tid);
+		if (tid == 0){
+			nthreads = omp_get_num_threads();
+			printf("Number of threads = %d\n", nthreads);
+		}
+		
+	}
+	return 0;
+}
