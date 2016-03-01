84c69e63081b067024d8b4c1b583909208a97597
ben <levinsky@seawolf.sonoma.edu>
2016-03-01 14:29:38 -0800

    update c file
    pragma's now don't conflict with compilation of file
    
    figured out a way to potentially map thread id into a region of the array
    
    TO DO: set number of threads to work with (should be equal to number of sectors in array, in this case  4 for 4 regions )

--- openMp_test.c
+++ openMp_test.c
@@ -2,37 +2,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ROW 2;
-#define COL 2;
+#define ROW 2
+#define COL 2
+#define SECTOR_HEIGHT 2
+#define SECTOR_WIDTH 2
 
+// given row r and column c and thread ID t_id, you can map thread ID into region of array using following formula                         
+// array(r,c) = array[r+m*t_id][c+m*t_id]                                                                                                  
 void secureAccess(int array[2][2], int threadID){
-	// 1. Map threadID to location in array
-	int row = threadID / ROW;
-	int col = threadID % COL;
-	
-	// 2. Modify array based on this mapping
-	printf("array value: %d, row: %d, col: %d, ThreadID: %d\n"
-		, array[row][col], row, col, threadID);
+        // 1. Map threadID to location in array                                                                                            
+        int row = threadID / ROW;
+        int col = threadID % COL;
+
+        // 2. Modify array based on this mapping                                                                                           
+        printf("array value: %d, row: %d, col: %d, ThreadID: %d\n" , array[row][col], row, col, threadID);
 
 }
 
 int main(int argc, char *argv[]){
-	int nthreads, tid;
-	int array[ROW][COL] = {
-		{0, 0},
-		{0, 0}
-	};
+        int nthreads, tid;
+        int array[ROW][COL] = {
+                {0, 0},
+                {0, 0}
+        };
 
-	#pragma omp parallel private(nthreads, tid)
-	{
+        #pragma omp parallel private(nthreads, tid)
+        {
 
-	tid = omp_get_thread_num();
-	printf("Hello World from thead = %d\n", tid);
-		if (tid == 0){
-			nthreads = omp_get_num_threads();
-			printf("Number of threads = %d\n", nthreads);
-		}
-	secureAccess(array, tid);
-	}
-	return 0;
+        tid = omp_get_thread_num();
+        printf("Hello World from thead = %d\n", tid);
+                if (tid == 0){
+                        nthreads = omp_get_num_threads();
+                        printf("Number of threads = %d\n", nthreads);
+                }
+        secureAccess(array, tid);
+        }
+        return 0;
 }
